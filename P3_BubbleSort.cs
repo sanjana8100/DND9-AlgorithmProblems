6d7143bde00155188fdebfdfa679160c1bc8e8e2
sanjana8100 <sanjanams812@gmail.com>
2023-06-18 23:50:41 +0530

    added: P3

---
+++ P3_BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND9_AlgorithmProblems
+{
+    internal class P3_BubbleSort
+    {
+        public static void Bubblesort(int[] array)
+        {
+            for (int index = 0; index < array.Length; index++)
+            {
+                for (int index2 = index + 1; index2 < array.Length; index2++)
+                {
+                    if (array[index] > array[index2])
+                    {
+                        int temp = array[index];
+                        array[index] = array[index2];
+                        array[index2] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("The Elements in the Array after performing Bubble Sort: ");
+            for (int index = 0; index < array.Length; index++)
+            {
+                Console.Write(array[index] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,9 +8,13 @@
             //Console.WriteLine("--------------------------------------------");
             //P1_BinarySearch.BinarySearch(new string[] { "abc", "xyz", "efg", "pqr" }, "efg");
 
-            Console.WriteLine("P2- Insertion Sort");
+            //Console.WriteLine("P2- Insertion Sort");
+            //Console.WriteLine("--------------------------------------------");
+            //P2_InsertionSort.InsertionSort(new int[] { 89, 63, 95, 27, 41, 75 });
+
+            Console.WriteLine("P3- Bubble Sort");
             Console.WriteLine("--------------------------------------------");
-            P2_InsertionSort.InsertionSort(new int[] { 89, 63, 95, 27, 41, 75 });
+            P3_BubbleSort.Bubblesort(new int[] { 50, 30, 70, 40, 10, 90, 20 });
         }
     }
 }
