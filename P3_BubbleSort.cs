23d1c5d9bb21cce2c8e88d52e6d10875e969da46
sanjana8100 <sanjanams812@gmail.com>
2023-06-27 01:03:25 +0530

    added: P3

--- P3_BubbleSort.cs
+++ P3_BubbleSort.cs
@@ -12,13 +12,13 @@ namespace DND9_AlgorithmProblems
         {
             for (int index = 0; index < array.Length; index++)
             {
-                for (int index2 = index + 1; index2 < array.Length; index2++)
+                for (int index2 = 0; index2 < array.Length - index - 1; index2++)
                 {
-                    if (array[index] > array[index2])
+                    if (array[index2] > array[index2 + 1])
                     {
-                        int temp = array[index];
-                        array[index] = array[index2];
-                        array[index2] = temp;
+                        int temp = array[index2];
+                        array[index2] = array[index2 + 1];
+                        array[index2 + 1] = temp;
                     }
                 }
             }
--- Program.cs
+++ Program.cs
@@ -12,12 +12,9 @@
             //Console.WriteLine("--------------------------------------------");
             //P2_InsertionSort.InsertionSort(new int[] { 89, 63, 95, 27, 41, 75 });
 
-            //Console.WriteLine("P3- Bubble Sort");
-            //Console.WriteLine("--------------------------------------------");
-            //P3_BubbleSort.Bubblesort(new int[] { 50, 30, 70, 40, 10, 90, 20 });
-
-            Console.WriteLine("P4- Merge Sort");
+            Console.WriteLine("P3- Bubble Sort");
             Console.WriteLine("--------------------------------------------");
+            P3_BubbleSort.Bubblesort(new int[] { 50, 30, 70, 40, 10, 90, 20 });
         }
     }
 }
